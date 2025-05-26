763fdffac14edc105d1f968a6f5e5afbb5444de1
bilal57070 <biilal.safi.57@gmail.com>
2025-05-26 20:02:26 +0200

    '

--- srcs/requirements/mariadb/tools/sc.sh
+++ srcs/requirements/mariadb/tools/sc.sh
@@ -1,6 +1,6 @@
 service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \'$(SQL_DATABASE)\';"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
